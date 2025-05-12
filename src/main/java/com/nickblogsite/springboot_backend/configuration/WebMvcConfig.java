755b654a2f50ac59042f9ccc888fbd42233c083a
Nicholas Germanos <ngermanos97@gmail.com>
2025-05-13 08:40:04 +1000

    favicon changes

--- src/main/java/com/nickblogsite/springboot_backend/configuration/WebMvcConfig.java
+++ src/main/java/com/nickblogsite/springboot_backend/configuration/WebMvcConfig.java
@@ -1,10 +1,16 @@
 package com.nickblogsite.springboot_backend.configuration;
 
+import java.util.*;
+
 import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.*;
 
+import lombok.*;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+	private static final String ANY_MATCHER = "{path:(?!.*\\.).*}";
 
 	@Override
 	public void addResourceHandlers( final ResourceHandlerRegistry registry ) {
@@ -14,10 +20,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	}
 
 	@Override
-	public void addViewControllers( final ViewControllerRegistry registry ) {
+	public void addViewControllers( @NonNull final ViewControllerRegistry registry ) {
 
-		registry.addViewController( "/" )
-			.setViewName( "forward:/dist/index.html" );
+		final List<String> validPaths = new ArrayList<>();
+		validPaths.add("/");
+		validPaths.add("/" + ANY_MATCHER);
+		validPaths.add("/blog/" + ANY_MATCHER );
+		validPaths.add("/newEntryForm/" + ANY_MATCHER );
+
+		for (final String path : validPaths) {
+			registry.addViewController( path )
+				.setViewName( "forward:/dist/index.html" );
+		}
 	}
 
 }
