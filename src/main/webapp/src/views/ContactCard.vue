e6c39cf2b92ef66d9895ac9717fff3e436d8f4b3
Nicholas Germanos <ngermanos97@gmail.com>
2025-06-01 16:44:06 +1000

    update email address for contact

--- src/main/webapp/src/views/ContactCard.vue
+++ src/main/webapp/src/views/ContactCard.vue
@@ -4,7 +4,7 @@
             <div class="m-4">
                 <h2>Contact me</h2>
                 <h6>(ask me anything)</h6>
-                <h5>ngermanos97@gmail.com</h5>
+                <h5>ironick.mail@gmail.com</h5>
             </div>
         </div>
     </div>
